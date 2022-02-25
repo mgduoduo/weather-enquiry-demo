16d05b2eb0efd3c043521b63c4e081be98f91c7a
gq47193 <gq47193@ly.com>
2022-02-26 01:15:58 +0800

    weather enquiry demo

--- src/main/java/com/sample/service/impl/WeatherSearchServiceImpl.java
+++ src/main/java/com/sample/service/impl/WeatherSearchServiceImpl.java
@@ -1,12 +1,12 @@
 package com.sample.service.impl;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.sample.constant.WeatherConstant;
 import com.sample.domain.WeatherInfo;
 import com.sample.service.RegionService;
 import com.sample.service.WeatherService;
 import com.sample.util.JacksonUtil;
 import com.sample.util.OkHttpClientUtil;
-import com.sample.util.RateLimiterUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.util.Asserts;
 import org.slf4j.Logger;
@@ -17,8 +17,7 @@ import java.net.URL;
 import java.text.MessageFormat;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @description:
@@ -28,19 +27,26 @@ import java.util.concurrent.ConcurrentHashMap;
 public class WeatherSearchServiceImpl implements WeatherService {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private Map<String, Map<String, Set<String>>> regionMap = new ConcurrentHashMap<>();
+    public static final int MAX_TPS = 100;
+
+    public RateLimiter rateLimiter;
 
     private final RegionService regionService;
 
     public WeatherSearchServiceImpl() {
         regionService = new RegionServiceImpl();
+        rateLimiter = RateLimiter.create(MAX_TPS);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Optional<Double> getAccurateTemperature(String province, String city, String county) {
-        if (!RateLimiterUtil.tryAcquire()) {
+        if (!tryAcquire()) {
             log.warn("Request is out of limit");
-            //System.out.println("Request is out of limit");
             return Optional.empty();
         }
         validArgument(province, city, county);
@@ -52,11 +58,9 @@ public class WeatherSearchServiceImpl implements WeatherService {
                     && weatherInfo.getWeatherInfo() != null
                     && weatherInfo.getWeatherInfo().getTemp() != null) {
                 log.info("region:{}, temperature:{}", StringUtils.join(province, city, county), weatherInfo.getWeatherInfo().getTemp());
-                //System.out.println("region:" + StringUtils.join(province, city, county) + ", temperature:" + weatherInfo.getWeatherInfo().getTemp());
                 return Optional.of(weatherInfo.getWeatherInfo().getTemp());
             }
         } catch (IOException e) {
-            //System.out.println("error:" + e.getMessage());
             log.error(e.getMessage());
         }
         return Optional.empty();
@@ -73,5 +77,12 @@ public class WeatherSearchServiceImpl implements WeatherService {
         Asserts.check(cities.containsKey(city), "[city code:" + city + "] is invalid city in [province:" + province + "]!");
         Map<String, String> counties = regionService.getCounties(province, city);
         Asserts.check(counties.containsKey(county), "[county code:" + county + "] is invalid county in [province:" + province + "][city:" + city + "]!");
+    }
+
+    /**
+     * try to get the token
+     */
+    public boolean tryAcquire() {
+        return rateLimiter.tryAcquire();
     }
 }
